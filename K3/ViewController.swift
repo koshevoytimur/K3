ff3be7d1bfef2a587f650b2154a0bba713a19a74
Timur Koshevoy <tkoshevoy@rightandabove.com>
2020-01-22 16:26:10 +0200

    Initial Commit

---
+++ K3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  K3
+//
+//  Created by Timur Koshevoy on 22.01.2020.
+//  Copyright © 2020 Timur Koshevoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
